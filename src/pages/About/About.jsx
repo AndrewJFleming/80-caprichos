7856a513e6fb16e0f14d60659c3e180520104494
Andrew Fleming <66144906+AndrewJFleming@users.noreply.github.com>
2022-04-10 09:45:09 -0700

    Add About and Contact page components.

---
+++ src/pages/About/About.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { Container } from "react-bootstrap";
+
+const About = () => {
+  return (
+    <div className="page-comp-wrapper">
+      <Container>
+        <h1 className="page-component-title">About</h1>
+      </Container>
+    </div>
+  );
+};
+export default About;
---
+++ src/pages/Contact/Contact.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Container } from "react-bootstrap";
+
+const Contact = () => {
+  return (
+    <div className="page-comp-wrapper">
+      <Container>
+        <h1 className="page-component-title">Contact</h1>
+      </Container>
+    </div>
+  );
+};
+
+export default Contact;
