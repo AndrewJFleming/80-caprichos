eee1f585b019d90196bf1de23eba4c4dea78ad3b
Andrew Fleming <66144906+AndrewJFleming@users.noreply.github.com>
2022-04-10 09:46:42 -0700

    Add routes for About and Contact page components.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import { categoryData, backgroundColorData } from "./data";
 import { Routes, Route } from "react-router-dom";
 import SingleImage from "./pages/SingleImage/SingleImage";
 import ImageCategory from "./pages/ImageCategory/ImageCategory";
+import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
 
 function App() {
   const [categories, setCategories] = useState(categoryData);
@@ -34,6 +36,8 @@ function App() {
         />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
           <Route path="/caprichos/:imageId" element={<SingleImage />} />
           <Route path="/category/:catId" element={<ImageCategory />} />
         </Routes>
