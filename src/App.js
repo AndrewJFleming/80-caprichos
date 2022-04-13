035d504a73b1ea5d97639ca5b8d368cf2095a56f
Andrew Fleming <66144906+AndrewJFleming@users.noreply.github.com>
2022-04-13 11:54:29 -0700

    create state for allSubCats across parent categories and pass down to About and PrintCategory page components.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Home from "./pages/Home/Home";
 import VerticalMenu from "./components/VerticalMenu/VerticalMenu";
 import Header from "./components/Header/Header";
 import { otherLinks, categoryData, backgroundColorData } from "./data";
 import { Routes, Route } from "react-router-dom";
-import SingleImage from "./pages/SingleImage/SingleImage";
-import ImageCategory from "./pages/ImageCategory/ImageCategory";
+import SinglePrint from "./pages/SinglePrint/SinglePrint";
+import PrintCategory from "./pages/PrintCategory/PrintCategory";
 import About from "./pages/About/About";
 import Contact from "./pages/Contact/Contact";
 
 function App() {
-  const [categories, setCategories] = useState(categoryData);
+  const [allSubCats, setAllSubCats] = useState([]);
   const [backgroundColor, setBackgroundColor] = useState(
     backgroundColorData[0].color
   );
   const [vertMenuCollapsed, setVertMenuCollapsed] = useState(true);
+
+  useEffect(() => {
+    const mergedSubCats = [];
+    categoryData.forEach((category) =>
+      category.subCats.forEach((subCat) => mergedSubCats.push(subCat))
+    );
+    setAllSubCats(mergedSubCats);
+  }, []);
 
   const handleVertCollapse = () => {
     setVertMenuCollapsed(!vertMenuCollapsed);
@@ -25,7 +33,7 @@ function App() {
     <div className="app-wrapper">
       <VerticalMenu
         otherLinks={otherLinks}
-        categories={categories}
+        categories={categoryData}
         backgroundColor={backgroundColor}
         vertMenuCollapsed={vertMenuCollapsed}
         handleVertCollapse={handleVertCollapse}
@@ -38,13 +46,13 @@ function App() {
         />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route
-            path="/about"
-            element={<About themes={categories[0].subCats} />}
-          />
+          <Route path="/about" element={<About themes={allSubCats} />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/caprichos/:imageId" element={<SingleImage />} />
-          <Route path="/category/:catId" element={<ImageCategory />} />
+          <Route path="/caprichos/:imageId" element={<SinglePrint />} />
+          <Route
+            path="/category/:catId"
+            element={<PrintCategory allSubCats={allSubCats} />}
+          />
         </Routes>
       </div>
     </div>
